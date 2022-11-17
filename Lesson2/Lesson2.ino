5867e4e306499a1b465b7e1fabf8f427613fe3cb
Srihari293 <ssrihari2002@gmail.com>
2022-11-17 17:19:13 +1100

    Update Lesson2.ino

--- Lesson2/Lesson2.ino
+++ Lesson2/Lesson2.ino
@@ -1,3 +1,21 @@
+// Lesson 2: Light and LED
+
+// How does a traditional light bulb work?
+// Electricity --> Resistance --> Heat --> Red hot --> White hot --> Light
+// Issue: 
+// -- Resistors are heat emitting device, not a light emitting ones. 
+// -- Therefore there is a lot of heat in the room is a waste of electricity. 
+// -- To cool this heat off we use A.C which is also a waste of electricity. 
+
+// So what is the solution?
+// Before we look at that, lets revisit semiconductor devices first.
+
+// Solid state physics:
+// Conductors - Conducts electricity
+// Insulator - Does not conducts electricity
+// Semi-conductors - Can be both conducting and insulating based on the electrical signals passed into it. Can be tailored.
+
+// Note: The Arduino itself has small chips of silicon wafers which are essentially semiconductor material. Everything is based on semi-conductors
 void setup() {
   // put your setup code here, to run once:
 
